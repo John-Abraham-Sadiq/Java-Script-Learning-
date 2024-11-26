f86553b1ee76eb68d997135fb80eb3b8f55e3fb6
asp <asp@aptech.com>
2024-11-26 18:38:21 +0500

    1st Class

---
+++ app.js
@@ -0,0 +1,64 @@
+{var a = 5
+a = 6
+var a = 7;
+}
+console.log(a);
+
+{let J = 9
+J = 10
+
+console.log(J);
+}
+
+{const Z = 143
+console.log(Z);
+}
+
+let obj1 = {
+    name: "John",
+    age: 17
+}
+obj1.name = "Abraham"
+obj1.email= "abc"
+console.log(obj1);
+
+
+
+const obj2 = {
+    name: "John",
+    age: 17
+
+}
+obj2.name = "sadiq"
+obj2.email= "abc"
+console.log(obj2);
+
+
+
+//////////
+
+var student = {
+  name: "John",
+  age: 21
+};
+
+let score = 85;
+
+const messages = "Great job!";
+
+console.log(student);
+console.log(score);
+console.log(messages);
+
+var student = {
+  name: "Alice",
+  age: 22
+};
+
+let scores = 95;
+
+const message = "Excellent work!";
+
+console.log(student);
+console.log(scores);
+console.log(message);
